15277e0b2f33ae5840da1afad44f7d22004aa68e
mtgnorton <15726204663@163.com>
2023-05-29 15:55:03 +0800

    redis get 和 set 通过 watch 命令保证原子性,如果失败,进行重试

---
+++ redis/redis.go
@@ -0,0 +1,53 @@
+package redis
+
+import (
+	"context"
+	"errors"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+type RC struct {
+	maxRetry int
+	client   *redis.Client
+}
+
+func NewRC(client *redis.Client, maxRetry ...int) *RC {
+	m := 20
+	if len(maxRetry) == 0 {
+		m = maxRetry[0]
+	}
+	return &RC{
+		maxRetry: m,
+		client:   client,
+	}
+}
+
+type FuncValue func(currentValue interface{}) (newValue interface{})
+
+func (rc *RC) SetIfFailRetry(ctx context.Context, key string, funcValue FuncValue, expiration time.Duration) error {
+
+	txf := func(tx *redis.Tx) error {
+
+		currentValue, err := tx.Get(ctx, key).Bytes()
+		if err != nil && err != redis.Nil {
+			return err
+		}
+		_, err = tx.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
+			pipe.Set(ctx, key, funcValue(currentValue), expiration)
+			return nil
+		})
+		return err
+	}
+	for i := 0; i < rc.maxRetry; i++ {
+		err := rc.client.Watch(ctx, txf, key)
+		if err == nil {
+			return nil
+		}
+		if err == redis.TxFailedErr {
+			continue
+		}
+		return err
+	}
+	return errors.New("increment reached maximum number of retries")
+}
