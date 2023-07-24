505e216523d1d1660887513605c95a5a97697534
mtgnorton <15726204663@163.com>
2023-07-24 18:06:55 +0800

    Refactor RetryFixedInterval to return a single error instead of a slice of errors.

--- retry/retry.go
+++ retry/retry.go
@@ -2,6 +2,7 @@ package retry
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
@@ -9,7 +10,8 @@ import (
 // retryIntervals 重试间隔,如[]int{1,2,3} 表示第一次重试间隔1秒,第二次重试间隔2秒,第三次重试间隔3秒
 // retryPeriod 重试周期,如 retryPeriod=10,表示10秒内重试,超过10秒后,重试次数清零,如果为 0,重试完成后直接退出
 // callback 重试回调函数
-func RetryFixedInterval(ctx context.Context, retryIntervals []int64, retryPeriod int64, callback func(ctx context.Context)) {
+func RetryFixedInterval(ctx context.Context, retryIntervals []int64, retryPeriod int64, callback func(ctx context.Context) error) (r error) {
+
 	if len(retryIntervals) == 0 {
 		return
 	}
@@ -30,7 +32,9 @@ func RetryFixedInterval(ctx context.Context, retryIntervals []int64, retryPeriod
 				lastConnectTime = now
 			}
 		}
-		callback(ctx)
+		if err := callback(ctx); err != nil {
+			r = fmt.Errorf("%w", fmt.Errorf("%d times retry fail,error:%s", retryTimes, err.Error()))
+		}
 		// 休眠
 		time.Sleep(time.Second * time.Duration(retryIntervals[retryTimes]))
 		retryTimes++
