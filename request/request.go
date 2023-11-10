cec67655064405fd15b9551bf93b39af3c0b9926
mtgnorton <15726204663@163.com>
2023-11-10 12:14:55 +0800

    Modified to accommodate massive response

--- request/request.go
+++ request/request.go
@@ -1,13 +1,17 @@
 package request
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/pkg/errors"
-	"io"
 	"net/http"
 )
 
 // Get 发起GET请求
-func Get(url string, params ...map[string]string) (statusCode int, content []byte, err error) {
+// url:请求地址
+// maxScanTokenSizeMul:  bufio.MaxScanTokenSize 为 64kb,如果返回的数据超过64kb,需要调整此参数,比如设置为2,则最大支持128kb,以此类推
+// params:请求参数,可选
+func Get(url string, maxScanTokenSizeMul int, params ...map[string]string) (statusCode int, content []byte, err error) {
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -15,27 +19,35 @@ func Get(url string, params ...map[string]string) (statusCode int, content []byt
 	}
 	q := req.URL.Query()
 
-	if len(params) > 0 {
+	if len(params) == 1 {
 		for k, v := range params[0] {
 			q.Add(k, v)
 		}
 		req.URL.RawQuery = q.Encode()
 	}
+
 	var resp *http.Response
 	resp, err = http.DefaultClient.Do(req)
 	if err != nil {
 		return 0, nil, errors.Wrapf(err, "http.DefaultClient.Do err,url:%v", url)
 	}
-
-	body, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		return 0, nil, errors.Wrapf(err, "io.ReadAll err,url:%v", url)
+	//body, err := io.ReadAll(resp.Body)
+	respContent := make([]byte, 0)
+	scanner := bufio.NewScanner(resp.Body)
+	buf := make([]byte, 0, bufio.MaxScanTokenSize*maxScanTokenSizeMul)
+	scanner.Buffer(buf, cap(buf))
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		respContent = append(respContent, line...)
 	}
-
+	fmt.Println(len(respContent))
+	if scanner.Err() != nil {
+		return 0, nil, errors.Wrapf(err, "scanner.Err err,url:%v", url)
+	}
 	err = resp.Body.Close()
+
 	if err != nil {
 		return 0, nil, errors.Wrapf(err, "body close err")
 	}
-	return resp.StatusCode, body, nil
+	return resp.StatusCode, respContent, nil
 }
--- request/request_test.go
+++ request/request_test.go
@@ -3,14 +3,12 @@ package request
 import "testing"
 
 func Test_Get(t *testing.T) {
-	code, r, err := Get("https://www.baidu.com")
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(code, string(r))
-	code, r1, err := Get("https://search.mtapi.io/Search", map[string]string{
-		"company": "Zora Capital Group Limited",
-	})
+	//code, r, err := Get("https://www.baidu.com")
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//t.Log(code, string(r))
+	code, r1, err := Get("https://search.mtapi.io/Search", 20)
 	if err != nil {
 		t.Fatal(err)
 	}
